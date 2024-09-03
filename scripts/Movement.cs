38a4cd95a911bb4cfaa7a6068288c785995cbdf2
killerwolf162 <tjongman.123@gmail.com>
2024-09-03 16:03:04 +0200

    basic scripts that are needed

---
+++ scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private int movementSpeed;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+}
